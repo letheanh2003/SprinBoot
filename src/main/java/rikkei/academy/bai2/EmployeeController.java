f827f719a0ef4de4b398392c8c4ad27448978c4b
Lê Thế Anh <letheanhbn2003@gmail.com>
2023-07-07 15:32:52 +0700

    Update

--- src/main/java/rikkei/academy/bai2/EmployeeController.java
+++ src/main/java/rikkei/academy/bai2/EmployeeController.java
@@ -1,43 +1,43 @@
 package rikkei.academy.bai2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @RestController
-@RequestMapping("/api")
 public class EmployeeController {
-    List<Employee> employeeslist = new ArrayList<>();
+    List<Employee> employees = new ArrayList<>();
 
     @GetMapping("/employee")
     public ResponseEntity<?> getAll() {
-        return new ResponseEntity<>(employeeslist, HttpStatus.OK);
+        return new ResponseEntity<>(employees, HttpStatus.OK);
     }
 
     @PostMapping("/employee")
     public ResponseEntity<?> create(@RequestBody Employee employee) {
-        if (employeeslist.size() == 0) {
-            employee.setId(1);
-            employeeslist.add(employee);
-            return new ResponseEntity<>(employee, HttpStatus.CREATED);
-        }
         if (findByEmail(employee.getEmail())) {
-            return new ResponseEntity<>("email existed ,please try again!!!!!!", HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>("Email already exists, please try again!", HttpStatus.BAD_REQUEST);
         }
-        employee.setId(employeeslist.get(employeeslist.size() - 1).getId() + 1);
-        employeeslist.add(employee);
+
+        if (employees.size() == 0) {
+            employee.setId(1);
+        } else {
+            employee.setId(employees.get(employees.size() - 1).getId() + 1);
+        }
+
+        employees.add(employee);
         return new ResponseEntity<>(employee, HttpStatus.CREATED);
     }
 
     @PutMapping("/employee")
-    public ResponseEntity<?> upDate(@RequestBody Employee employee) {
+    public ResponseEntity<?> update(@RequestBody Employee employee) {
         if (findByEmail(employee.getEmail())) {
-            return new ResponseEntity<>("email existed ,please try again!!!!!!", HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>("Email already exists, please try again!", HttpStatus.BAD_REQUEST);
         }
-        for (Employee e : employeeslist) {
+        for (Employee e : employees) {
             if (e.getId() == employee.getId()) {
                 e.setEmail(employee.getEmail() != null ? employee.getEmail() : e.getEmail());
                 e.setName(employee.getName() != null ? employee.getName() : e.getName());
@@ -46,31 +46,35 @@ public class EmployeeController {
                 return new ResponseEntity<>(e, HttpStatus.OK);
             }
         }
+
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @DeleteMapping("/employee/{id}")
     public ResponseEntity<?> delete(@PathVariable("id") int id) {
-        for (int i = 0; i < employeeslist.size(); i++) {
-            if (employeeslist.get(i).getId() == id) {
-                employeeslist.remove(i);
-                return new ResponseEntity<>(HttpStatus.OK);
+        for (int i = 0; i < employees.size(); i++) {
+            if (employees.get(i).getId() == id) {
+                employees.remove(i);
+                return new ResponseEntity<>("Delete Success", HttpStatus.OK);
             }
         }
+
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @GetMapping("/employee/{id}")
     public ResponseEntity<?> findById(@PathVariable("id") int id) {
-        for (Employee e : employeeslist) {
-            if (e.getId() == id) ;
-            return new ResponseEntity<>(e, HttpStatus.OK);
+        for (Employee e : employees) {
+            if (e.getId() == id) {
+                return new ResponseEntity<>(e, HttpStatus.OK);
+            }
         }
+
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     public boolean findByEmail(String email) {
-        for (Employee e : employeeslist) {
+        for (Employee e : employees) {
             if (e.getEmail().equals(email)) {
                 return true;
             }
